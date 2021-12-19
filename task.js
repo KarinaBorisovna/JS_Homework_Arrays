df141f2e64a6fe1022b0896535613ad1f86fc01b
Karina Vinogradova <x-mower@mail.ru>
2021-12-19 20:04:34 +0300

    task.js added

---
+++ task.js
@@ -0,0 +1,14 @@
+function compareArrays(arr1, arr2) {
+  let result;
+
+  result = (arr1.length === arr2.length) && (arr1.every((value, index) => value === arr2[index]));
+
+  return result;
+}
+
+function advancedFilter(arr) {
+
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+  
+  return resultArr; 
+}
